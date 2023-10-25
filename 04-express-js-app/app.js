be5b08fbb011eb36bcc4d211a4a33d82c6675d07
sekharmisra <59437658+sekharmisra@users.noreply.github.com>
2023-10-25 12:49:57 -0400

    Commited express app - Learnt Routers and 404 Error codes!

--- 04-express-js-app/app.js
+++ 04-express-js-app/app.js
@@ -4,27 +4,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/',(req, res, next) => {
-    console.log("In default middleware which gets executed automatically everytime page loads!");
-    next();
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In /add-product middleware');
-    res.send('<html><body><form action="/product" method="POST"><input type="text" name="product"></input><button type="submit">Add Product</button></form></body></html>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log('In /product middleware');
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In / middleware');
-    res.send('<h1>Welcome to Sekhars Website</h1>');
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Not found</h1>");
 });
 
 app.listen(8080);
