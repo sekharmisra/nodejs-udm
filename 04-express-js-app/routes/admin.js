17f29578dd640a508f91c4cdb29abe5bd26ade59
sekharmisra <59437658+sekharmisra@users.noreply.github.com>
2023-10-26 13:05:43 -0400

    Commited Express.JS App. Pausing Development of Section

--- 04-express-js-app/routes/admin.js
+++ 04-express-js-app/routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+//admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     console.log('In /add-product middleware');
-    res.send('<html><body><form action="/product" method="POST"><input type="text" name="product"></input><button type="submit">Add Product</button></form></body></html>');
+    res.send('<html><body><form action="/admin/product" method="POST"><input type="text" name="product"></input><button type="submit">Add Product</button></form></body></html>');
 });
 
+//admin/add-product => POST
 router.post('/product', (req, res, next) => {
     console.log('In /product middleware');
     console.log(req.body);
